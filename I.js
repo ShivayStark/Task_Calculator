cbc2f6c73887c1cae15682f5c1e2c99fdff89524
A Shiv <44468979+ShivayStark@users.noreply.github.com>
2021-08-31 16:53:17 +0530

    Add files via upload

---
+++ I.js
@@ -0,0 +1,31 @@
+let number = document.getElementById('number');
+buttons = document.querySelectorAll('button');
+let numbervalue = '';
+for(item of buttons){
+
+    item.addEventListener('click', (b)=>{
+
+        buttontext = b.target.innerText; 
+        console.log('button value is', buttontext);
+        //logic
+        if(buttontext=='='){
+            number.value = eval(numbervalue);
+            
+            
+        }
+        else if(buttontext == 'c'){
+            numbervalue = "";           
+            number.value = numbervalue;
+        }
+        else if( buttontext == 'X'){
+            numbervalue = number.value;
+            numbervalue = numbervalue.substring(0,numbervalue.length - 1);
+            number.value = numbervalue;
+        }
+        else{
+            numbervalue += buttontext;
+            number.value = numbervalue;
+        }
+     })
+
+}
